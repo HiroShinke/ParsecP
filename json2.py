d4741e2a437a075f43794360ba97f9f2cc7fa59d
新家博文 <hiro@hiro-no-MacBook-puro.local>
2018-06-02 18:54:45 +0900

    a JSON Parser ported from https://github.com/sighingnow/parsec.py/blob/master/examples/jsonc.py

---
+++ json2.py
@@ -0,0 +1,72 @@
+
+
+
+from parsecp2 import *
+import operator
+import sys
+import re
+
+def tr(reg): return token(pR(reg))
+def ts(str): return token(pS(str))
+
+def const(v): return (lambda t: v)
+
+def createParser():
+
+    lbrace = ts("{")
+    rbrace = ts("}")
+    lbrack = ts("[")
+    rbrack = ts("]")
+
+    colon  = ts(":")
+    comma  = ts(",")
+    true   = ts("true")  >> const(True)
+    false  = ts("false") >> const(False)
+    null   = ts("null")  >> const(None)
+    number = tr('-?(0|[1-9][0-9]*)([.][0-9]+)?([eE][+-]?[0-9]+)?') >> \
+             ( lambda t: float(t.word) )
+
+    string_part = pR(r'[^"\\]') >> (lambda t: t.word)
+    string_esc  = k(ts('\\')) + ( pS("\\")>> const("\\") | 
+                                  pS("/") >> const("/")  |
+                                  pS('"') >> const('"')  | 
+                                  pS("b") >> const("\b") |
+                                  pS("f") >> const("\f") |
+                                  pS("n") >> const("\n") |
+                                  pS("r") >> const("\r") |
+                                  pS("t") >> const("\t") |
+                                  pR(r'u[0-9a-fA-F]{4}') >> (lambda t: char(int(t.word[1:],16))))
+
+    quoted = k(ts('"')) + m(string_part | string_esc) + k(pS('"')) > \
+             (lambda *ts: "".join(ts) )
+
+    array = para(lbrack, sb(pRef(lambda : value), comma), rbrack) > \
+             (lambda *vs: [v for v in vs] )
+
+    object_pair = quoted + colon + pRef(lambda : value) > \
+                  (lambda k,c,v: (k,v))
+
+    json_obj = para(lbrace, sb(object_pair, comma), rbrace) > \
+              (lambda *vs: dict(vs) )
+
+    value = quoted | number | json_obj | array | true | false | null
+
+    return value
+
+def mainLoop():
+    pExpr = createParser()
+    buff = ""
+    while True:
+        str = sys.stdin.readline()
+        buff = buff + str
+        m = re.search(r";",buff)
+        if m:
+            pos = m.start(0)
+            print( "buff={}".format(buff[0:pos]) )
+            print( runParser(pExpr,buff[0:pos]) )
+            buff = ""
+
+
+if __name__ == "__main__":
+    mainLoop()
+
---
+++ json2test.py
@@ -0,0 +1,115 @@
+
+
+from json2 import *
+import unittest
+
+class Json2Test(unittest.TestCase):
+
+    def test_number(self):
+
+        p = createParser()
+        success,s,v = runParser(p,"""
+1
+""")
+        self.assertTrue(v == 1.0)
+        
+        success,s,v = runParser(p,"""
+        1
+""")
+        self.assertTrue(v == 1.0)
+
+        success,s,v = runParser(p,"""
+        1.0
+""")
+        self.assertTrue(v == 1.0)
+        
+
+    def test_literal(self):
+
+        p = createParser()
+        success,s,v = runParser(p,"""
+"abcdef"
+""")
+        self.assertTrue(v == "abcdef")
+        
+        success,s,v = runParser(p,"""
+        "abcdef"
+""")
+        self.assertTrue(v == "abcdef")
+
+
+    def test_literal2(self):
+
+        p = createParser()
+        success,s,v = runParser(p,r"""
+"abcdef\\"
+""")
+        self.assertTrue(v == "abcdef\\")
+        
+        success,s,v = runParser(p,r"""
+"abcdef\/"
+""")
+        self.assertTrue(v == "abcdef/")
+
+        
+    def test_array(self):
+        
+        p = createParser()
+        success,s,v = runParser(p,"""
+[1,2,3,4,5]
+""")
+        self.assertTrue(v == [1.0,2.0,3.0,4.0,5.0])
+
+        success,s,v = runParser(p,"""
+[1, 2, 3, 4, 5]
+""")
+        self.assertTrue(v == [1.0,2.0,3.0,4.0,5.0])
+
+        success,s,v = runParser(p,"""
+[1 ,2 ,3 ,4 ,5]
+""")
+        self.assertTrue(v == [1.0,2.0,3.0,4.0,5.0])
+
+    def test_nest_array(self):
+        
+        p = createParser()
+        success,s,v = runParser(p,"""
+[1,2,[3,4],5]
+""")
+        self.assertTrue(v == [1.0,2.0,[3.0,4.0],5.0])
+
+
+    def test_obj(self):
+        
+        p = createParser()
+        success,s,v = runParser(p,"""
+{ 
+  "abc" : 10,
+  "efg" : 100 
+}
+""")
+        self.assertTrue(v == { "abc" : 10.0, "efg" : 100.0 } )
+
+        success,s,v = runParser(p,"""
+{ 
+  "abc" : "abc",
+  "efg" : "def" 
+}
+""")
+        self.assertTrue(v == { "abc" : "abc", "efg" : "def" } )
+
+        success,s,v = runParser(p,"""
+{ 
+        "abc" : [1,2,3],
+        "efg" : [4,5,6]
+}
+""")
+        self.assertTrue(v == { "abc" : [1.0,2.0,3.0],
+                               "efg" : [4.0,5.0,6.0] } )
+
+
+unittest.main()
+
+
+
+    
